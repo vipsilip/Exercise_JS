3b00ab3eb1e1fae20ffed15d42f6ec6efa7e40ff
Le Nguyen Thanh Hai <thanhhai.hvdn@gmail.com>
2022-11-06 22:43:51 +0700

    linkedList-pending insertBeforePosition

---
+++ src/Bonus.js
@@ -0,0 +1,27 @@
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function createArrayRandom(n) {
+  const result = [];
+  let subArr = [];
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      let random = Math.floor(Math.random() * 100) + 1;
+      while (subArr.includes(random) || result.find((x) => x.includes(random))) {
+        random = Math.floor(Math.random() * 100) + 1;
+      }
+      if (isPrime(random)) console.log(`[${i}][${j}]`);
+      subArr.push(random);
+      if (subArr.length > n) {
+        result.push(subArr);
+        subArr = [];
+      }
+    }
+  }
+  console.log(result);
+}
+console.log(createArrayRandom(5));
