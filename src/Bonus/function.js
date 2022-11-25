013d0ba76ffc5163eed77a22f67884d36aa02575
ThanhHai <thanhhai.hvdn@gmail.com>
2022-11-25 17:14:51 +0700

    25/12/2022

---
+++ src/Bonus/function.js
@@ -0,0 +1,283 @@
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+function bai4(n) {
+  if (n <= 0) return -1;
+  for (let i = 1; i < n; i++) {
+    if (isPrime(i)) process.stdout.write(`${i} `);
+  }
+}
+bai4(100);
+
+function isPerfectSquare(n) {
+  return Math.sqrt(n) === Math.trunc(Math.sqrt(n)) ? true : false;
+}
+function bai5(n) {
+  if (n <= 0) return -1;
+  for (let i = 1; i <= n; i++) {
+    if (isPerfectSquare(i)) process.stdout.write(`${i} `);
+  }
+}
+bai5(100);
+
+function isPerfectNumber(n) {
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) sum += i;
+  }
+  return sum === n ? true : false;
+}
+function bai6(n) {
+  if (n <= 0) return -1;
+  let count = 0;
+  for (let i = 1; i < n; i++) {
+    if (isPerfectNumber(i)) count++;
+  }
+  return count;
+}
+console.log(bai6(28));
+
+function bai7(n) {
+  if (n < 0 || n >= 1000) return undefined;
+  if (n === 0) process.stdout.write(`khong`);
+  const hundred = Math.trunc(n / 100);
+  const ten = Math.trunc(n / 10) % 10;
+  const zero = n % 10;
+  let result = '';
+  switch (hundred) {
+    case 1:
+      result += `một trăm `;
+      break;
+    case 2:
+      result += `hai trăm `;
+      break;
+    case 3:
+      result += `ba trăm `;
+      break;
+    case 4:
+      result += `bốn trăm `;
+      break;
+    case 5:
+      result += `năm trăm `;
+      break;
+    case 6:
+      result += `sáu trăm `;
+      break;
+    case 7:
+      result += `bảy trăm `;
+      break;
+    case 8:
+      result += `tám trăm `;
+      break;
+    case 9:
+      result += `chín trăm `;
+      break;
+  }
+  switch (ten) {
+    case 1:
+      result += `mười `;
+      break;
+    case 2:
+      result += `hai mươi `;
+      break;
+    case 3:
+      result += `ba mươi `;
+      break;
+    case 4:
+      result += `bốn mươi `;
+      break;
+    case 5:
+      result += `năm mươi `;
+      break;
+    case 6:
+      result += `sáu mươi `;
+      break;
+    case 7:
+      result += `bảy mươi `;
+      break;
+    case 8:
+      result += `tám mươi `;
+      break;
+    case 9:
+      result += `chín mươi `;
+      break;
+    case 0:
+      hundred === 0 ? (result += '') : (result += `lẻ `);
+      break;
+  }
+  switch (zero) {
+    case 1:
+      ten === 1 ? (result += `một`) : (result += `mốt`);
+      break;
+    case 2:
+      result += `hai`;
+      break;
+    case 3:
+      result += `ba`;
+      break;
+    case 4:
+      result += `bốn`;
+      break;
+    case 5:
+      result += `năm`;
+      break;
+    case 6:
+      result += `sáu`;
+      break;
+    case 7:
+      result += `bảy`;
+      break;
+    case 8:
+      result += `tám`;
+      break;
+    case 9:
+      result += `chín`;
+      break;
+    case 0:
+      result += '';
+      break;
+  }
+
+  return result;
+}
+console.log(bai7(105));
+console.log(bai7(0));
+console.log(bai7(9));
+console.log(bai7(110));
+console.log(bai7(91));
+console.log(bai7(126));
+console.log(bai7(999));
+
+function bai8(startTime, endTime, number) {
+  if (
+    startTime < 6 ||
+    startTime > endTime ||
+    endTime > 21 ||
+    !Number.isInteger(startTime) ||
+    !Number.isInteger(endTime)
+  )
+    return undefined;
+  let money = 0;
+  if (endTime < 17.5) return (money = (endTime - startTime) * 2500 * number);
+  if (startTime > 17.5) return (money = (endTime - startTime) * 3000 * number);
+  return (money = (17.5 - startTime) * 2500 + (endTime - 17.5) * 3000) * number;
+}
+console.log(bai8(10, 21, 2));
+
+function bai9(timeIn, timeOut) {
+  if (timeIn < 6 || timeOut > 18) return undefined;
+  let money;
+  if (timeIn >= 12) return (money = (timeOut - timeIn) * 7500);
+  if (timeOut < 12) return (money = (timeOut - timeIn) * 6000);
+  return (money = (12 - timeIn) * 6000 + (timeOut - 12) * 7500);
+}
+console.log(bai9(6, 17));
+console.log(bai9(6, 12));
+console.log(bai9(13, 17));
+
+function bai11(a, b, c) {
+  if (a < 0 || b < 0 || c < 0) return 'khong lap thanh tam giac';
+  if (a + b > c && a + c > b && b + c > a) {
+    const p = (a + b + c) / 2;
+    const s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    const ha = (2 * s) / a;
+    const hb = (2 * s) / b;
+    const hc = (2 * s) / c;
+    return `${s},${ha},${hb},${hc}`;
+  }
+  return 'khong lap thanh tam giac';
+}
+console.log(bai11(8, 7, 9));
+
+function bai12(a, b, c, d, e, f) {
+  let x, y, D, Dx, Dy;
+  D = a * e - d * b;
+  Dx = c * e - f * b;
+  Dy = a * f - d * c;
+
+  if (D === 0) {
+    if (Dx === 0 && Dy === 0) console.log('PT co vo so nghiem');
+    else console.log('PT vo nghiem');
+  } else {
+    x = Dx / D;
+    y = Dy / D;
+    console.log(x, y);
+  }
+}
+bai12(-2, 1, 5, 1, 3, 1);
+bai12(1, -2, 4, -3, 6, -12);
+function bai13(a, b) {
+  const max = Math.max(a, b);
+  let bcnn;
+  for (let i = max; i <= a * b; i += max) {
+    if (i % a === 0 && i % b === 0) {
+      bcnn = i;
+      break;
+    }
+  }
+  console.log(bcnn);
+  let temp;
+  while (b !== 0) {
+    temp = a % b;
+    a = b;
+    b = temp;
+  }
+  console.log(`UCLN la: ${a}`);
+}
+console.log(bai13(5, 15));
+console.log(bai13(10, 1));
+
+function bai14(n) {
+  let sum = 0;
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    sum += 1 / (factorial *= i);
+  }
+  return sum;
+}
+console.log(bai14(4));
+
+function bai15(a, b) {
+  //   let temp;
+  //   temp = a;
+  //   a = b;
+  //   b = temp;
+  //   return `${a}-${b}`;
+  a = a * b;
+  b = a / b;
+  a = a / b;
+  return `${a}-${b}`;
+}
+console.log(bai15(4, 9));
+
+function bai16(n) {
+  if (n >= 100000) return false;
+  const tenThousand = Math.trunc(n / 10000) === 0 ? null : Math.trunc(n / 10000);
+  const thousand = Math.trunc(n / 1000) % 10 === 0 ? null : Math.trunc(n / 1000) % 10;
+  const hundred = Math.trunc(n / 100) % 10 === 0 ? null : Math.trunc(n / 100) % 10;
+  const ten = Math.trunc(n / 10) % 10;
+  const zero = n % 10;
+  if (n > 9999) return tenThousand === zero && thousand === ten ? true : false;
+  if (n > 999) return thousand === zero && hundred === ten ? true : false;
+  if (n > 99) return hundred === zero ? true : false;
+  if (n > 9) return ten === zero ? true : false;
+  if (n > 0) return true;
+  return false;
+}
+console.log(bai16(12321));
+console.log(bai16(12345));
+console.log(bai16(1221));
+console.log(bai16(1224));
+console.log(bai16(121));
+console.log(bai16(129));
+console.log(bai16(11));
+console.log(bai16(17));
+console.log(bai16(1));
+
+function bai17(n) {
+  if (n < 0 || n > 100000) return -1;
+  let count = 0;
+}
--- src/Bonus/if_else.js
+++ src/Bonus/if_else.js
@@ -175,136 +175,656 @@ console.log(bai15(9));
 console.log(bai15(8));
 console.log(18 % 2);
 
-const a = 18;
-for (let i = 1; i <= a; i++) {
-  if (a % i === 0) console.log(i);
-}
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j <= i; j++) {
-    process.stdout.write(`${j}`);
-  }
-  console.log('\n');
-}
+// const a = 18;
+// for (let i = 1; i <= a; i++) {
+//   if (a % i === 0) console.log(i);
+// }
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     process.stdout.write(`${j}`);
+//   }
+//   console.log('\n');
+// }
 
-let i = 10;
-let s = 0;
-while (i > 0) {
-  if (i % 2 === 0) s += i;
-  else {
-    if (i > 5) s += 2 * i;
-  }
-  i--;
-}
-console.log(`s= ${s}`);
+// let i = 10;
+// let s = 0;
+// while (i > 0) {
+//   if (i % 2 === 0) s += i;
+//   else {
+//     if (i > 5) s += 2 * i;
+//   }
+//   i--;
+// }
+// console.log(`s= ${s}`);
 
-let a = 18;
-let i = 1;
-do {
-  if (a % i === 0) console.log(i);
-  i++;
-} while (i <= a);
+// let a = 18;
+// let i = 1;
+// do {
+//   if (a % i === 0) console.log(i);
+//   i++;
+// } while (i <= a);
 
-let a = 11,
-  b = 16,
-  i = a;
-while (i < b) {
-  if (i % 2 === 0) {
-    console.log(i);
-    break;
-  }
-  i++;
-}
+// let a = 11,
+//   b = 16,
+//   i = a;
+// while (i < b) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//     break;
+//   }
+//   i++;
+// }
 
-let a = 10,
-  s = 0,
-  i = 0;
-while (i < a) {
-  i++;
-  if (i % 2 === 0) continue;
-  else s += i;
-}
-console.log(s);
+// let a = 10,
+//   s = 0,
+//   i = 0;
+// while (i < a) {
+//   i++;
+//   if (i % 2 === 0) continue;
+//   else s += i;
+// }
+// console.log(s);
 
-let i = 1,
-  s = 0;
-while (1) {
-  s += i++;
-  if (i % 2) i += 2;
-  else i += 1;
-  if (i > 20) break;
-}
-console.log(s);
+// let i = 1,
+//   s = 0;
+// while (1) {
+//   s += i++;
+//   if (i % 2) i += 2;
+//   else i += 1;
+//   if (i > 20) break;
+// }
+// console.log(s);
+// ------------------------------------------------------------------------------------
 
-function bai23(m, n) {
+function hinhChuNhat(m, n) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
       process.stdout.write(`* `);
     }
-    process.stdout.write(`\n`);
+    process.stdout.write('\n');
   }
 }
-bai23(5, 4);
-function bai24(m, n) {
+hinhChuNhat(5, 4);
+
+function hinhChuNhatRong(m, n) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-      if (i === 0 || i === n - 1) process.stdout.write(`* `);
+      if (i === 0 || i === n - 1) process.stdout.write('* ');
       else {
-        if (j === 0 || j === m - 1) process.stdout.write(`* `);
-        else process.stdout.write(`  `);
+        j === 0 || j === m - 1 ? process.stdout.write('* ') : process.stdout.write('  ');
       }
+    }
+    process.stdout.write('\n');
+  }
+}
+hinhChuNhatRong(5, 4);
+
+function hinhTamGiacVuong(h) {
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j <= i; j++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+hinhTamGiacVuong(4);
+
+function hinhTamGiacVuongRong(h) {
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (i === 0 || i === h - 1) process.stdout.write(`* `);
+      else {
+        j === 0 || j === i ? process.stdout.write(`* `) : process.stdout.write(`  `);
+      }
+    }
+    process.stdout.write('\n');
+  }
+}
+hinhTamGiacVuongRong(4);
+
+function hinnhTamGiacCan(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = i; j <= h - 1; j++) {
+      process.stdout.write(`  `);
+    }
+    for (j = 1; j <= i * 2 - 1; j++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+hinnhTamGiacCan(4);
+
+function hinnhTamGiacCanRong(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = i; j <= h - 1; j++) {
+      process.stdout.write(`  `);
+    }
+    for (j = 1; j <= i * 2 - 1; j++) {
+      if (i === 1 || i === h) process.stdout.write('* ');
+      else {
+        j === 1 || j === i * 2 - 1 ? process.stdout.write(`* `) : process.stdout.write(`  `);
+      }
+    }
+    process.stdout.write('\n');
+  }
+}
+hinnhTamGiacCanRong(4);
+
+function TGC1(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = 1; j < i; j++) {
+      process.stdout.write(`  `);
+    }
+    for (let k = h - i; k >= 0; k--) {
+      process.stdout.write(`* `);
+    }
+    process.stdout.write('\n');
+  }
+}
+TGC1(5);
+
+function TGC2(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = h - i; j > 0; j--) {
+      process.stdout.write('  ');
+    }
+    for (let k = 0; k < i; k++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+TGC2(5);
+function TGC3(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = 0; j <= h - i; j++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+TGC3(5);
+function TGC4(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+TGC4(5);
+function hinhCanhQuat(h) {
+  for (let i = 1; i <= h; i++) {
+    for (let a = 1; a < i; a++) {
+      process.stdout.write(`  `);
+    }
+    for (let b = h - i; b >= 0; b--) {
+      process.stdout.write(`* `);
+    }
+    for (let c = h - i; c > 0; c--) {
+      process.stdout.write('  ');
+    }
+    for (let d = 0; d < i; d++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+  for (i = 1; i <= h; i++) {
+    for (let e = 0; e <= h - i; e++) {
+      process.stdout.write('* ');
+    }
+    for (let f = 1; f < i; f++) {
+      process.stdout.write('  ');
+    }
+    for (let g = 1; g <= i; g++) {
+      process.stdout.write('* ');
+    }
+    process.stdout.write('\n');
+  }
+}
+hinhCanhQuat(2);
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2 || n === 3) return true;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+function bai29(n) {
+  let count = 0;
+  let i = 2;
+  while (count < n) {
+    if (isPrime(i)) {
+      console.log(i);
+      count++;
+    }
+    i++;
+  }
+}
+bai29(10);
+
+function divisorList(n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) result.push(i);
+  }
+  return result;
+}
+
+function bai30(a, b) {
+  let bcnn;
+  const max = Math.max(a, b);
+  for (let i = max; i <= a * b; i += max) {
+    if (i % a === 0 && i % b === 0) {
+      bcnn = i;
+      break;
+    }
+  }
+  console.log(`BCNN: ${bcnn}`);
+
+  let temp;
+  while (b !== 0) {
+    temp = a % b;
+    a = b;
+    b = temp;
+  }
+  console.log(`UCLN: ${a}`);
+}
+bai30(12, 25);
+
+function bai31(n) {
+  if (n <= 0 || n > 10000000000) return undefined;
+  let binaryList = [];
+  for (let i = 0; n > 0; i++) {
+    binaryList[i] = n % 2;
+    n = Math.trunc(n / 2);
+  }
+  for (let i = binaryList.length - 1; i >= 0; i--) {
+    // process.stdout.write(`${binaryList[i]}`);
+    console.log(binaryList[i]);
+  }
+}
+bai31(1234567890);
+
+function bai32(n) {
+  if (n <= 0) return -1;
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) count++;
+  }
+  return count;
+}
+console.log(bai32(25));
+
+function isPerfectNum(n) {
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) sum += i;
+  }
+  return sum === n ? true : false;
+}
+function bai33(n) {
+  // if (n > 5000) return undefined;
+  for (let i = 1; i <= n; i++) {
+    if (isPerfectNum(i)) console.log(i);
+  }
+}
+bai33(10000);
+
+function bai34(day, month, year) {
+  let countDay = 0;
+  for (let i = 1; i < month; i++) {
+    switch (i) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        countDay += 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        countDay += 30;
+        break;
+      case 2:
+        if (year % 4 === 0 || (year % 100 === 0 && year % 400 === 0)) {
+          countDay += 29;
+        } else {
+          countDay += 28;
+        }
+        break;
+    }
+  }
+  return (countDay += day);
+}
+console.log(bai34(21, 3, 2022));
+
+function fibonacci(n) {
+  let f0 = 0;
+  let f1 = 1;
+  let fn = 1;
+  if (n < 0) return 0;
+  if (n === 1 || n === 0) return n;
+  for (let i = 2; i < n; i++) {
+    f0 = f1;
+    f1 = fn;
+    fn = f0 + f1;
+  }
+  return fn;
+}
+console.log(fibonacci(2));
+function bai35(n) {
+  // if (n === 1 || n === 0) return 1;
+  // const fiboList = [1, 1];
+  // for (let i = 2; i < n; i++) {
+  //   fiboList[i] = fiboList[i - 1] + fiboList[i - 2];
+  // }
+  // return fiboList.join(',');
+  for (let i = 0; i < n; i++) {
+    process.stdout.write(`${fibonacci(i)} `);
+  }
+}
+bai35(10);
+
+function bai37(day, month, year) {
+  if (day < 1 || day > 31 || month < 1 || month > 12) return 'ko hop le';
+  switch (month) {
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (day === 30) {
+        day = 1;
+        month++;
+        break;
+      }
+      day++;
+      break;
+    case 2:
+      if (year % 4 === 0 || (year % 100 === 0 && year % 400 === 0)) {
+        if (day === 29) {
+          day = 1;
+          month++;
+          break;
+        }
+        day++;
+        break;
+      } else {
+        if (day === 28) {
+          day = 1;
+          month++;
+          break;
+        }
+        day++;
+        break;
+      }
+    default:
+      if (day === 31 && month === 12) {
+        day = 1;
+        month = 1;
+        year++;
+        break;
+      }
+      if (day === 31) {
+        day = 1;
+        month++;
+        break;
+      }
+      day++;
+      break;
+  }
+  return `${day}/${month}/${year}`;
+}
+console.log(bai37(31, 12, 2003));
+console.log(bai37(28, 2, 2000));
+console.log(bai37(28, 2, 2003));
+
+function isLeapYear(year) {
+  return year % 4 === 0 || (year % 100 === 0 && year % 400 === 0) ? true : false;
+}
+function lastOfDay(day, month, year) {
+  // let lastDay;
+  switch (month) {
+    case 1:
+    case 2:
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (day === 1) day = 31;
+      else day--;
+      break;
+    case 3:
+      if (day === 1) {
+        if (isLeapYear(year)) day = 29;
+        else day = 28;
+      } else day--;
+      break;
+    default:
+      if (day === 1) day = 30;
+      else day--;
+      break;
+  }
+  return day;
+}
+function lastOfMonth(day, month) {
+  // let lastDay;
+  switch (month) {
+    case 1:
+      if (day === 1) {
+        month = 12;
+        break;
+      }
+    default:
+      if (day === 1) {
+        month--;
+        break;
+      }
+  }
+  return month;
+}
+function lastOfYear(day, month, year) {
+  // let lastDay;
+  if (day === 1 && month === 1) year--;
+  return year;
+}
+function bai38(day, month, year) {
+  if (day < 1 || day > 31 || month < 1 || month > 12) return 'ko hop le';
+  return (
+    lastOfDay(day, month, year) + '/' + lastOfMonth(day, month) + '/' + lastOfYear(day, month, year)
+  );
+}
+console.log(bai38(1, 1, 2003));
+console.log(bai38(1, 3, 2000));
+console.log(bai38(1, 3, 2003));
+console.log(bai38(31, 12, 2003));
+
+// CACH 1
+function bai39(day, month, year) {
+  let countDay = 0;
+  for (let i = 1; i < month; i++) {
+    switch (i) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        countDay += 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        countDay += 30;
+        break;
+      case 2:
+        if (year % 4 === 0 || (year % 100 === 0 && year % 400 === 0)) {
+          countDay += 29;
+        } else {
+          countDay += 28;
+        }
+        break;
+    }
+  }
+  countDay += day;
+  while (countDay > 7) {
+    countDay = countDay - 7;
+  }
+
+  switch (countDay) {
+    case 1:
+      console.log(`${day}/${month}/${year} la ngay thu 7`);
+      break;
+    case 2:
+      console.log(`${day}/${month}/${year} la ngay CN`);
+      break;
+    case 3:
+      console.log(`${day}/${month}/${year} la ngay thu 2`);
+      break;
+    case 4:
+      console.log(`${day}/${month}/${year} la ngay thu 3`);
+      break;
+    case 5:
+      console.log(`${day}/${month}/${year} la ngay thu 4`);
+      break;
+    case 6:
+      console.log(`${day}/${month}/${year} la ngay thu 5`);
+      break;
+    case 7:
+      console.log(`${day}/${month}/${year} la ngay thu 6`);
+      break;
+  }
+}
+// CACH 2
+function bai39(day, month, year) {
+  let countDay = 0;
+  for (let i = 1; i < month; i++) {
+    switch (i) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        countDay += 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        countDay += 30;
+        break;
+      case 2:
+        if (year % 4 === 0 || (year % 100 === 0 && year % 400 === 0)) {
+          countDay += 29;
+        } else {
+          countDay += 28;
+        }
+        break;
+    }
+  }
+  countDay += day;
+  const S = Math.trunc(
+    (year - 1 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400 + countDay) % 7
+  );
+  console.log(S);
+  switch (S) {
+    case 0:
+      console.log(`${day}/${month}/${year} la ngay CN`);
+      break;
+    case 1:
+      console.log(`${day}/${month}/${year} la ngay thu 2`);
+      break;
+    case 2:
+      console.log(`${day}/${month}/${year} la ngay thu 3`);
+      break;
+    case 3:
+      console.log(`${day}/${month}/${year} la ngay thu 4`);
+      break;
+    case 4:
+      console.log(`${day}/${month}/${year} la ngay thu 5`);
+      break;
+    case 5:
+      console.log(`${day}/${month}/${year} la ngay thu 6`);
+      break;
+    case 6:
+      console.log(`${day}/${month}/${year} la ngay thu 7`);
+      break;
+  }
+}
+bai39(24, 1, 2003);
+bai39(28, 2, 2022);
+bai39(2, 3, 2022);
+bai39(30, 4, 2022);
+bai39(15, 5, 2022);
+bai39(30, 6, 2022);
+bai39(31, 7, 2022);
+bai39(4, 8, 2022);
+bai39(21, 9, 2022);
+bai39(10, 10, 2022);
+bai39(21, 11, 2022);
+bai39(4, 12, 2022);
+
+function bai40(hours, minutes, seconds) {
+  if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+    return undefined;
+  if (seconds === 59) {
+    seconds = 0;
+    minutes += 1;
+  } else seconds += 1;
+  if (minutes > 59) {
+    minutes = 0;
+    hours += 1;
+  }
+  if (hours > 23) {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+  }
+  return `0${hours}`.slice(-2) + ':' + `0${minutes}`.slice(-2) + ':' + `0${seconds}`.slice(-2);
+}
+console.log(bai40(23, 59, 59));
+console.log(bai40(4, 59, 50));
+console.log(bai40(10, 46, 18));
+
+function bai41(hours, minutes, seconds) {
+  if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+    return undefined;
+  if (seconds === 0) {
+    seconds = 59;
+    minutes -= 1;
+  } else seconds -= 1;
+  if (minutes < 0) {
+    minutes = 59;
+    hours -= 1;
+  }
+  if (hours < 0) {
+    hours = 23;
+    minutes = 59;
+    seconds = 59;
+  }
+  return `0${hours}`.slice(-2) + ':' + `0${minutes}`.slice(-2) + ':' + `0${seconds}`.slice(-2);
+}
+console.log(bai41(2, 0, 0));
+console.log(bai41(4, 12, 0));
+console.log(bai41(10, 0, 18));
+
+function bai42() {
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 2; j <= 9; j++) {
+      process.stdout.write(`${i} x ${j} = ${j * i} |`);
     }
     process.stdout.write(`\n`);
   }
 }
-bai24(5, 4);
-
-function bai25(h) {
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j <= i; j++) {
-      process.stdout.write('* ');
-    }
-    process.stdout.write('\n');
-  }
-}
-bai25(5);
-function bai26(h) {
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j <= i; j++) {
-      if (j === 0 || j === i) process.stdout.write('* ');
-      else {
-        if (i === 0 || i === h - 1) process.stdout.write('* ');
-        else process.stdout.write('  ');
-      }
-    }
-    process.stdout.write('\n');
-  }
-}
-bai26(5);
-function bai27(h) {
-  for (i = 1; i <= h; i++) {
-    for (j = 0; j < h - i; j++) {
-      process.stdout.write('  ');
-    }
-    for (j = 1; j <= 2 * i - 1; j++) {
-      process.stdout.write('* ');
-    }
-    process.stdout.write('\n');
-  }
-}
-bai27(4);
-function bai28(h) {
-  for (i = 1; i <= h; i++) {
-    for (j = 0; j < h - i; j++) {
-      process.stdout.write('  ');
-    }
-    for (j = 1; j <= 2 * i - 1; j++) {
-      if (i === 1 || i === h) process.stdout.write('* ');
-      else {
-        if (j === 1 || j >= 2 * i - 1) process.stdout.write('* ');
-        else process.stdout.write('  ');
-      }
-    }
-    process.stdout.write('\n');
-  }
-}
-bai28(4);
+bai42();
